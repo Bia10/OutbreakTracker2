a644e2da97a6c96f82be4068eb3cd65becc1615e
Bia10 <t.locke1234@gmail.com>
2025-05-27 21:50:44 +0200

    feat: add dialogs for scenario entitites

---
+++ src/app/OutbreakTracker2.App/Views/Dashboard/ClientOverview/InGameScenario/Entitites/ScenarioEntitiesViewModel.cs
@@ -0,0 +1,95 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using OutbreakTracker2.Outbreak.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace OutbreakTracker2.App.Views.Dashboard.ClientOverview.InGameScenario.Entitites;
+
+public partial class ScenarioEntitiesViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<DecodedItem> _items = [];
+
+    [ObservableProperty]
+    private ObservableCollection<DecodedEnemy> _enemies = [];
+
+    public void UpdateItems(DecodedItem[] newItems)
+    {
+        List<DecodedItem> newItemsList = newItems.ToList();
+
+        for (int i = Items.Count - 1; i >= 0; i--)
+        {
+            DecodedItem existingItem = Items[i];
+            if (!newItemsList.Any(newItem => newItem.SlotIndex == existingItem.SlotIndex && newItem.Id == existingItem.Id))
+                Items.RemoveAt(i);
+        }
+
+        foreach (DecodedItem newItem in newItemsList)
+        {
+            DecodedItem? existingItem = Items.FirstOrDefault(existingItem => existingItem.SlotIndex == newItem.SlotIndex && existingItem.Id == newItem.Id);
+            if (existingItem == null)
+                Items.Add(newItem);
+            else
+            {
+                int index = Items.IndexOf(existingItem);
+                if (index is -1)
+                    continue;
+
+                if (existingItem.En != newItem.En ||
+                    existingItem.TypeName != newItem.TypeName ||
+                    existingItem.Quantity != newItem.Quantity ||
+                    existingItem.PickedUp != newItem.PickedUp ||
+                    existingItem.Present != newItem.Present ||
+                    existingItem.Mix != newItem.Mix ||
+                    existingItem.RoomId != newItem.RoomId)
+                {
+                    Items[index] = newItem;
+                }
+            }
+        }
+    }
+
+    public void UpdateEnemies(DecodedEnemy[] newEnemies)
+    {
+        List<DecodedEnemy> newEnemiesList = newEnemies.ToList();
+
+        for (int i = Enemies.Count - 1; i >= 0; i--)
+        {
+            DecodedEnemy existingEnemy = Enemies[i];
+            if (!newEnemiesList.Any(newEnemy => newEnemy.SlotId == existingEnemy.SlotId && newEnemy.Id == existingEnemy.Id))
+                Enemies.RemoveAt(i);
+        }
+
+        foreach (DecodedEnemy newEnemy in newEnemiesList)
+        {
+            DecodedEnemy? existingEnemy = Enemies.FirstOrDefault(e => e.SlotId == newEnemy.SlotId && e.Id == newEnemy.Id);
+
+            if (existingEnemy is null)
+            {
+                Enemies.Add(newEnemy);
+            }
+            else
+            {
+                int index = Enemies.IndexOf(existingEnemy);
+                if (index is -1)
+                    continue;
+
+                if (existingEnemy.Enabled != newEnemy.Enabled ||
+                    existingEnemy.InGame != newEnemy.InGame ||
+                    existingEnemy.RoomId != newEnemy.RoomId ||
+                    existingEnemy.TypeId != newEnemy.TypeId ||
+                    existingEnemy.NameId != newEnemy.NameId ||
+                    existingEnemy.Name != newEnemy.Name ||
+                    existingEnemy.CurHp != newEnemy.CurHp ||
+                    existingEnemy.MaxHp != newEnemy.MaxHp ||
+                    existingEnemy.BossType != newEnemy.BossType ||
+                    existingEnemy.Status != newEnemy.Status ||
+                    existingEnemy.RoomName != newEnemy.RoomName)
+                {
+                    Enemies[index] = newEnemy;
+                }
+            }
+        }
+    }
+}
--- src/app/OutbreakTracker2.App/Views/Dashboard/ClientOverview/InGameScenario/InGameScenarioViewModel.cs
+++ src/app/OutbreakTracker2.App/Views/Dashboard/ClientOverview/InGameScenario/InGameScenarioViewModel.cs
@@ -1,22 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 using OutbreakTracker2.App.Services.Data;
 using OutbreakTracker2.App.Services.Dispatcher;
+using OutbreakTracker2.App.Views.Dashboard.ClientOverview.InGameScenario.Entitites;
 using OutbreakTracker2.App.Views.Dashboard.ClientOverview.InGameScenario.FileOne;
 using OutbreakTracker2.App.Views.Dashboard.ClientOverview.InGameScenario.FileTwo;
 using OutbreakTracker2.Outbreak.Enums;
 using OutbreakTracker2.Outbreak.Models;
 using OutbreakTracker2.Outbreak.Utility;
 using R3;
+using SukiUI.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OutbreakTracker2.App.Views.Dashboard.ClientOverview.InGameScenario;
 
 public partial class InGameScenarioViewModel : ObservableObject
 {
     private readonly ILogger<InGameScenarioViewModel> _logger;
+    private readonly IDispatcherService _dispatcherService;
     private readonly Dictionary<Scenario, Action<DecodedInGameScenario>> _scenarioUpdateActions;
+
+    private ISukiDialogManager DialogManager { get; }
 
     [ObservableProperty]
     private byte _currentFile;
@@ -106,15 +113,22 @@ public partial class InGameScenarioViewModel : ObservableObject
     [ObservableProperty]
     private ObservableObject? _currentScenarioSpecificViewModel;
 
+    [ObservableProperty]
+    private ScenarioEntitiesViewModel _scenarioEntitiesVm;
+
     public bool IsScenarioActive => FrameCounter > 0;
     public bool IsScenarioNotActive => FrameCounter <= 0;
 
     public InGameScenarioViewModel(
         ILogger<InGameScenarioViewModel> logger,
         IDataManager dataManager,
+        ISukiDialogManager dialogManager,
         IDispatcherService dispatcherService)
     {
         _logger = logger;
+        _dispatcherService = dispatcherService;
+        DialogManager = dialogManager;
+
         DesperateTimesViewModel desperateTimesVm = new();
         EndOfTheRoadViewModel endOfTheRoadVm = new();
         UnderbellyViewModel underbellyVm = new();
@@ -124,6 +138,7 @@ public partial class InGameScenarioViewModel : ObservableObject
         DecisionsDecisionsViewModel decisionsDecisionsVm = new();
         BelowFreezingPointViewModel belowFreezingPointVm = new();
 
+        _scenarioEntitiesVm = new ScenarioEntitiesViewModel();
         _scenarioUpdateActions = new Dictionary<Scenario, Action<DecodedInGameScenario>>
         {
             { Scenario.DesperateTimes, scenario => { desperateTimesVm.Update(scenario); CurrentScenarioSpecificViewModel = desperateTimesVm; } },
@@ -158,6 +173,50 @@ public partial class InGameScenarioViewModel : ObservableObject
                     _logger.LogError(ex, "Error during inGame scenario data processing cycle");
                 }
             }, AwaitOperation.Drop);
+
+        dataManager.EnemiesObservable.ObserveOnThreadPool()
+            .SubscribeAwait(async (enemies, cancellationToken) =>
+            {
+                _logger.LogTrace("Processing enemies data on thread pool");
+                try
+                {
+                    await dispatcherService.InvokeOnUIAsync(() =>
+                    {
+                        ScenarioEntitiesVm.UpdateEnemies(enemies);
+                    }, cancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }, AwaitOperation.Drop);
+    }
+
+    [RelayCommand]
+    private Task ShowItemsDialogAsync()
+    {
+        return _dispatcherService.InvokeOnUIAsync(() =>
+        {
+            DialogManager.CreateDialog()
+                .WithTitle("Scenario Items")
+                .WithContent(new ScenarioItemsView { DataContext = ScenarioEntitiesVm })
+                .WithActionButton("Close ", _ => { }, true)
+                .TryShow();
+        });
+    }
+    
+    [RelayCommand]
+    private Task ShowEnemiesDialogAsync()
+    {
+        return _dispatcherService.InvokeOnUIAsync(() =>
+        {
+            DialogManager.CreateDialog()
+                .WithTitle("Scenario Enemies")
+                .WithContent(new ScenarioEnemiesView { DataContext = ScenarioEntitiesVm })
+                .WithActionButton("Close ", _ => { }, true)
+                .TryShow();
+        });
     }
 
     public void Update(DecodedInGameScenario scenario)
@@ -189,6 +248,8 @@ public partial class InGameScenarioViewModel : ObservableObject
         GameTimeDisplay = GetGameTime();
         GasRandomOrderDisplay = CalculateGasRandomOrderDisplay();
         IsCleared = GetClearedDisplay();
+
+        ScenarioEntitiesVm.UpdateItems(scenario.Items);
 
         UpdateScenarioSpecificViewModel(scenario);
     }
