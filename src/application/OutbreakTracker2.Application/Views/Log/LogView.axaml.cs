8afb48245dc3cb4e5b27e223e082f5ae0332c943
Bia10 <t.locke1234@gmail.com>
2025-06-06 23:07:47 +0200

    chore: add git omited file

---
+++ src/application/OutbreakTracker2.Application/Views/Log/LogView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OutbreakTracker2.Application.Views.Log;
+
+public partial class LogView : UserControl
+{
+    public LogView()
+    {
+        InitializeComponent();
+    }
+}
