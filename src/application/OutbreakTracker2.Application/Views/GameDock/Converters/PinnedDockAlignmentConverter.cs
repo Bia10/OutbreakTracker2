ea29e4068c0c586d8b16ab7adbe17e721cebdc67
Bia10 <t.locke1234@gmail.com>
2026-04-11 15:53:41 +0200

    feat: add PinnedDock converters and update dock control themes

---
+++ src/application/OutbreakTracker2.Application/Views/GameDock/Converters/PinnedDockAlignmentConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Dock.Model.Controls;
+using Dock.Model.Core;
+
+namespace OutbreakTracker2.Application.Views.GameDock.Converters;
+
+public sealed class PinnedDockAlignmentConverter : IValueConverter
+{
+    public static readonly PinnedDockAlignmentConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is IToolDock dock ? dock.Alignment : Alignment.Left;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        new BindingNotification(new InvalidOperationException(), BindingErrorType.Error);
+}
---
+++ src/application/OutbreakTracker2.Application/Views/GameDock/Converters/PinnedDockHasVisibleContentConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Dock.Model.Controls;
+
+namespace OutbreakTracker2.Application.Views.GameDock.Converters;
+
+public sealed class PinnedDockHasVisibleContentConverter : IValueConverter
+{
+    public static readonly PinnedDockHasVisibleContentConverter Instance = new();
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is IToolDock dock && !dock.IsEmpty;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        new BindingNotification(new InvalidOperationException(), BindingErrorType.Error);
+}
